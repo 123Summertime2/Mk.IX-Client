82a1f27197c8409bb332b22ad9229f43d1c26aa9
123Summertime2 <3391189744@qq.com>
2025-01-27 11:00:40 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -22,5 +22,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: mode === 'github-pages' ? '/Mk.IX-Client/' : './',
+  base: process.env.VITE_BASE || './',
 })
